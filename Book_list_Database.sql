6225f4fff949789c120d8778eb81d73094f381d1
eliteops <62295367+eliteops@users.noreply.github.com>
2023-06-19 18:52:21 +0530

    Create Book_list_Database.sql

---
+++ Book_list_Database.sql
@@ -0,0 +1,4 @@
+CREATE TABLE book_list_database(id integer ,name  TEXT,rating INTEGER);                  # Creating Columns of INTEGER, TEXT Datatypes
+
+insert into book_list_database values(1,'harshit', 3),(2, 'harry', 4), (3, 'potter', 5); 
+SELECT * FROM book_list_database;
